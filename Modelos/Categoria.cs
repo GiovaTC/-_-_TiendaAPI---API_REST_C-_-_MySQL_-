12c880877e1aac994823b4987c7039408051a383
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-03 11:16:19 -0500

    commit - Categoria / .

--- Modelos/Categoria.cs
+++ Modelos/Categoria.cs
@@ -1,6 +1,16 @@
-﻿namespace TiendaAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaAPI.Modelos
 {
     public class Categoria
     {
-    }
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;  
+
+        public ICollection<Producto> Productos { get; set; }
+            = new List<Producto>();
+    }   
 }
