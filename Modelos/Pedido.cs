79da7c21f00f6219dbecad957ecad89b9bed1588
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-03 11:07:01 -0500

    commit - Pedido .

--- Modelos/Pedido.cs
+++ Modelos/Pedido.cs
@@ -1,6 +1,16 @@
-﻿namespace TiendaAPI.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaAPI.Modelos
 {
     public class Pedido
     {
-    }
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public int ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+        public ICollection<DetallePedido> Detalles { get; set; }
+            = new List<DetallePedido>();    
+    }   
 }
