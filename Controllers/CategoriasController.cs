c4d77191050a96cbac8fca76e8050beecec92615
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-08 10:12:20 -0500

    commit - CategoriasController .

---
+++ Controllers/CategoriasController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaAPI.Data;
+using TiendaAPI.Modelos;
+
+namespace TiendaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            var categorias = await _context.Categorias
+                .Include(c => c.Productos)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _context.Categorias
+                .Include(c => c.Productos)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (categoria == null)
+                return NotFound();
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetCategoria), 
+                new { id = categoria.Id }, 
+                categoria);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.Id)
+                return BadRequest();
+
+            _context.Entry(categoria).State =
+                EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria(int id)
+        {
+            var categoria = await _context.Categorias
+                .FindAsync(id);
+
+            if (categoria == null)
+                return NotFound();
+
+            _context.Categorias.Remove(categoria);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return Conflict(new
+                {
+                    mensaje =
+                    "No se puede eliminar la categoría porque tiene productos asociados ."
+                });
+            }
+
+            return NoContent();
+        }
+    }
+}
