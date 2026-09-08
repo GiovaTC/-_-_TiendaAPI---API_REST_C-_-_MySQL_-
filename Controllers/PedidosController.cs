1718f2fc4b042afbe66d31bf0bf15da88ad1f42c
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-08 09:37:00 -0500

    commit - PedidosController .

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -28,6 +28,52 @@ namespace TiendaAPI.Controllers
                 .ToListAsync();
 
             return Ok(pedidos);
-        }   
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedido>> GetPedido(int id)
+        {
+            var pedido = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .Include(p => p.Detalles)
+                    .ThenInclude(d => d.Producto)
+                        .ThenInclude(p => p!.Categoria)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pedido == null)
+                return NotFound(new
+                {
+                    mensaje = "pedido no encontrado"
+                });
+
+            return Ok(pedido);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pedido>> PostPedido(Pedido pedido)
+        {
+            var clienteExistente = await _context.Clientes
+                .AnyAsync(c => c.Id == pedido.ClienteId);
+
+            if (!clienteExistente)
+            { 
+                return BadRequest(new
+                {
+                    mensaje = "el cliente no existe"
+                });
+            }
+
+            pedido.Fecha = DateTime.Now;
+            _context.Pedidos.Add(pedido);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetPedido),
+                new { id = pedido.Id },
+                pedido);
+        }
+
     }
 }
