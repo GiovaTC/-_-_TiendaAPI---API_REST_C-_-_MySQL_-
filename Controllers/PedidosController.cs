b8d0318698058788999754065660fc165e6cc5e4
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-08 09:30:33 -0500

    commit - PedidosController / .

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -1,6 +1,33 @@
-﻿namespace TiendaAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaAPI.Data;
+using TiendaAPI.Modelos;
+
+namespace TiendaAPI.Controllers
 {
-    public class PedidosController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PedidosController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public PedidosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pedido>>> GetPedidos()
+        {
+            var pedidos = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .Include(p => p.Detalles)
+                    .ThenInclude(d => d.Producto)
+                        .ThenInclude(p => p!.Categoria)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(pedidos);
+        }   
     }
 }
